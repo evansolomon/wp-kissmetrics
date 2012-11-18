52f12faf7855a67cdc21ae6743150ee198b45695
Evan Solomon <evan@evanalyze.com>
2012-11-18 00:26:53 -0800

    Rewrite JS in coffescript
    kissmetrics.js is now the compiled output of kissmetrics.coffee

---
+++ kissmetrics.coffee
@@ -0,0 +1,65 @@
+# Fallback for Kissmetrics global
+window._kmq = window._kmq || []
+
+# Helper functions
+( ( $, global ) ->
+	global.kissmetrics =
+
+		# Script loader
+		_kms: ( url ) ->
+			setTimeout ->
+				d = document
+				f = d.getElementsByTagName( 'script' )[0]
+
+				s = d.createElement 'script'
+				$.extend s,
+					type : 'text/javascript'
+					async: true
+					src  : url
+
+				f.parentNode.insertBefore s, f
+			, 1
+
+		# name should be a string
+		# e.g. 'Clicked signup button'
+		# properties should be an array of key/value pair objects
+		# e.g. [ { experiment: 'domain form' }, { version: 'original' } ]
+		recordEvent: ( name, properties = {} )->
+			prefixedProperties = {}
+			$.each properties, ( property, value ) ->
+				prefixedProperties[ "#{name}|#{property}" ] = value;
+
+			_kmq.push [ 'record', name, prefixedProperties ]
+
+		# Property should be a single key/value pair object
+		# e.g. { location: 'San Francisco' }
+		setProperty: ( property ) ->
+			_kmq.push [ 'set', property ]
+
+		# Handles API setup and events/properties in the DOM on ready
+		init: ->
+			kissmetrics = window.kissmetrics_api || {}
+			queries     = window.kissmetrics_queries || {}
+			events      = queries.events || {}
+			properties  = queries.properties || {}
+
+			# // Need this to send data anywhere
+			return null if !kissmetrics
+
+			@_kms '//i.kissmetrics.com/i.js'
+			@_kms "//doug1izaerwt3.cloudfront.net/#{kissmetrics.api_key}.1.js"
+
+			# Use wp usernames to identify users
+			_kmq.push [ 'identify', kissmetrics.username ] if kissmetrics.username
+
+			# Record arbitrary events
+			$.each events, ->
+				global.kissmetrics.recordEvent @name, @properties
+
+			# Set arbitrary properties
+			$.each properties, ->
+				global.kissmetrics.setProperty @
+
+	# Bind init to $(document).ready() in its current context
+	$( $.proxy global.kissmetrics.init, global.kissmetrics )
+)( jQuery, window )
--- kissmetrics.js
+++ kissmetrics.js
@@ -1,78 +1,56 @@
-// Make sure the 'kissmetrics' global object exists
-window.kissmetrics = window.kissmetrics || {};
+window._kmq = window._kmq || [];
 
-// From Kissmetrics' default JS
-var _kmq = _kmq || [];
-function _kms(u){
-	setTimeout(function(){
-		var d = document,
-			f = d.getElementsByTagName( 'script' )[ 0 ],
-			s = d.createElement( 'script' );
-
-		s.type  = 'text/javascript';
-		s.async = true;
-		s.src   = u;
-
-		f.parentNode.insertBefore( s, f );
-	}, 1);
-}
-
-// Helper functions
-(function($){
-	kissmetrics = {
-		// name should be a string
-		// e.g. 'Clicked signup button'
-		// properties should be an array of key/value pair objects
-		// e.g. [ { experiment: 'domain form' }, { version: 'original' } ]
-		recordEvent: function( name, properties ) {
-			var prefixedProperties = {};
-			properties = properties || {};
-
-			$.each( properties, function( property, value ) {
-				prefixedProperties[ name + ' | ' + property ] = value;
-			});
-
-			_kmq.push( [ 'record', name, prefixedProperties ] );
-		},
-
-		// Property should be a single key/value pair object
-		// e.g. { location: 'San Francisco' }
-		setProperty: function( property ) {
-			_kmq.push( [ 'set', property ] );
-		},
-
-		// Handles API setup and events/properties in the DOM on ready
-		init: function() {
-			var kissmetrics = window.kissmetrics_api || {},
-				queries = window.kissmetrics_queries || {},
-				events = queries.events || {},
-				properties = queries.properties || {};
-
-			// Remaining functions use data populated by kissmetrics_js() in PHP
-
-			// Need this to send data anywhere
-			if ( ! kissmetrics.api_key )
-				return;
-
-			_kms( '//i.kissmetrics.com/i.js' );
-			_kms( '//doug1izaerwt3.cloudfront.net/' + kissmetrics.api_key + '.1.js' );
-
-			// Use wp usernames to identify users
-			if ( kissmetrics.username )
-				_kmq.push( [ 'identify', kissmetrics.username ] );
-
-			// Record arbitrary events
-			$.each( events, function() {
-				kissmetrics.recordEvent( this.name, this.properties );
-			});
-
-			// Set arbitrary properties
-			$.each( properties, function() {
-				kissmetrics.setProperty( this );
-			});
-		}
-	};
-
-	// Bind init to $(document).ready()
-	$( kissmetrics.init );
-}(jQuery));
+(function($, global) {
+  global.kissmetrics = {
+    _kms: function(url) {
+      return setTimeout(function() {
+        var d, f, s;
+        d = document;
+        f = d.getElementsByTagName('script')[0];
+        s = d.createElement('script');
+        $.extend(s, {
+          type: 'text/javascript',
+          async: true,
+          src: url
+        });
+        return f.parentNode.insertBefore(s, f);
+      }, 1);
+    },
+    recordEvent: function(name, properties) {
+      var prefixedProperties;
+      if (properties == null) {
+        properties = {};
+      }
+      prefixedProperties = {};
+      $.each(properties, function(property, value) {
+        return prefixedProperties["" + name + "|" + property] = value;
+      });
+      return _kmq.push(['record', name, prefixedProperties]);
+    },
+    setProperty: function(property) {
+      return _kmq.push(['set', property]);
+    },
+    init: function() {
+      var events, kissmetrics, properties, queries;
+      kissmetrics = window.kissmetrics_api || {};
+      queries = window.kissmetrics_queries || {};
+      events = queries.events || {};
+      properties = queries.properties || {};
+      if (!kissmetrics) {
+        return null;
+      }
+      this._kms('//i.kissmetrics.com/i.js');
+      this._kms("//doug1izaerwt3.cloudfront.net/" + kissmetrics.api_key + ".1.js");
+      if (kissmetrics.username) {
+        _kmq.push(['identify', kissmetrics.username]);
+      }
+      $.each(events, function() {
+        return global.kissmetrics.recordEvent(this.name, this.properties);
+      });
+      return $.each(properties, function() {
+        return global.kissmetrics.setProperty(this);
+      });
+    }
+  };
+  return $($.proxy(global.kissmetrics.init, global.kissmetrics));
+})(jQuery, window);
